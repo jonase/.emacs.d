78616ac34f80dc23a9fc63c6a99cfb796c346bb5
Jonas Enlund <jonas.enlund@gmail.com>
2016-05-01 14:40:49 +0300

    tweaks

--- init.el
+++ init.el
@@ -22,31 +22,31 @@
       mac-command-modifier 'meta
       x-select-enable-clipboard t)
 
-(unless (package-installed-p 'clojure-mode)
-  (package-install 'clojure-mode))
+(dolist (package '(clojure-mode
+		   cider
+		   company
+		   paredit
+		   zenburn-theme))
+  (unless (package-installed-p package)
+    (package-install package)))
 
-(unless (package-installed-p 'cider)
-  (package-install 'cider))
+(add-hook 'clojure-mode-hook
+	  (lambda ()
+	    (paredit-mode)
+	    (company-mode)))
 
-(unless (package-installed-p 'company)
-  (package-install 'company))
-
-(unless (package-installed-p 'paredit)
-  (package-install 'paredit))
-
-(add-hook 'clojure-mode-hook #'paredit-mode)
-(add-hook 'clojure-mode-hook #'company-mode)
-(add-hook 'cider-mode-hook #'company-mode)
-(add-hook 'cider-mode-hook #'eldoc-mode)
+(add-hook 'cider-mode-hook
+	  (lambda ()
+	    (company-mode)
+	    (eldoc-mode)))
 
 (setq cider-repl-pop-to-buffer-on-connect nil
       cider-prompt-for-symbol nil
       cider-prompt-save-file-on-load 'always-save)
 
-(unless (package-installed-p 'zenburn-theme)
-  (package-install 'zenburn-theme))
 (load-theme 'zenburn t)
 
 (setq ido-everywhere t
       ido-enable-flex-matching t)
+
 (ido-mode 1)
