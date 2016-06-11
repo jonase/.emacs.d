9a9edd2446029d8ac1cc3052be6aa900b05f04ba
Jonas Enlund <jonas.enlund@gmail.com>
2016-06-11 07:37:47 +0300

    ocaml config

--- init.el
+++ init.el
@@ -29,11 +29,21 @@
 		   cider
 		   company
 		   paredit
+		   utop
 		   dockerfile-mode
 		   zenburn-theme))
   (unless (package-installed-p package)
     (package-install package)))
 
+
+(load-theme 'zenburn t)
+
+(setq ido-everywhere t
+      ido-enable-flex-matching t)
+
+(ido-mode 1)
+
+;; Clojure
 (add-hook 'clojure-mode-hook
 	  (lambda ()
 	    (paredit-mode)
@@ -52,12 +62,6 @@
 
 (add-hook 'cider-repl-mode-hook #'paredit-mode)
 
-(add-hook 'emacs-lisp-mode-hook
-	  (lambda ()
-	    (paredit-mode)
-	    (company-mode)
-	    (eldoc-mode)))
-
 (add-hook 'before-save-hook
 	  (lambda ()
 	    (delete-trailing-whitespace)
@@ -70,9 +74,35 @@
       cider-prompt-for-symbol nil
       cider-prompt-save-file-on-load 'always-save)
 
-(load-theme 'zenburn t)
+;; Emacs lisp
+(add-hook 'emacs-lisp-mode-hook
+	  (lambda ()
+	    (paredit-mode)
+	    (company-mode)
+	    (eldoc-mode)))
 
-(setq ido-everywhere t
-      ido-enable-flex-matching t)
+;; Ocaml
+;; Note: Assumes that tuareg and merlin is installed via opam:
+;; $ opam install tuareg
+;; $ opam install merlin
+(setq opam-share
+      (substring (shell-command-to-string "opam config var share 2> /dev/null")
+		 0 -1))
 
-(ido-mode 1)
+(load (concat opam-share "/emacs/site-lisp/tuareg-site-file"))
+
+(add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+
+(require 'merlin)
+
+;; Use the opam installed utop
+(setq utop-command "opam config exec -- utop -emacs")
+
+(autoload 'utop-minor-mode "utop" "Minor mode for utop" t)
+
+(add-hook 'tuareg-mode-hook
+	  (lambda ()
+	    (merlin-mode)
+	    (utop-minor-mode)))
+
+(add-hook 'merlin-mode-hook 'company-mode)
